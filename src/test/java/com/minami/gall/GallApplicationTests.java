22200f3aba47a3c0d61dab75f766bd4c7fb3b918
Minami <dft2007@naver.com>
2023-11-17 20:10:16 +0900

    프로젝트 생성

---
+++ src/test/java/com/minami/gall/GallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minami.gall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
