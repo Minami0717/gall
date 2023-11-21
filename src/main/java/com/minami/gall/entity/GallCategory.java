31277465b46b4899cc340a9ec8754acfc35ce81a
Minami <dft2007@naver.com>
2023-11-22 00:43:14 +0900

    엔티티 작업중

---
+++ src/main/java/com/minami/gall/entity/GallCategory.java
@@ -0,0 +1,16 @@
+package com.minami.gall.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class GallCategory {
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    private Long categoryId;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String nm;
+}
