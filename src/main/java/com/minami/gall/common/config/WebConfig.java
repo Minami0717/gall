5f017754dea175b7e4abeee9718ee477ce9b732a
Minami <dft2007@naver.com>
2023-11-30 02:17:43 +0900

    이미지 경로 테스트

--- src/main/java/com/minami/gall/common/config/WebConfig.java
+++ src/main/java/com/minami/gall/common/config/WebConfig.java
@@ -1,18 +1,20 @@
-//package com.minami.gall.common.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    private String connectPath = "image/**";
-//    private String resourcePath = "file:///home/image";
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler(connectPath)
-//                .addResourceLocations(resourcePath);
-//    }
-//}
+package com.minami.gall.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${connect-path}")
+    private String connectPath;
+    @Value("${resource-path}")
+    private String resourcePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(connectPath)
+                .addResourceLocations(resourcePath);
+    }
+}
